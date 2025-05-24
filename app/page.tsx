acd39e0915d66dd9a0ac83df552aae68aa58d330
titanslayer2746 <yatin.2022ug1074@iiitranchi.ac.in>
2025-05-24 11:44:20 +0530

    Navbar and hero section  styling - 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import BgGradient from "@/components/common/BgGradient";
 import HeroSection from "@/components/home/HeroSection";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
@@ -5,7 +6,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="relative w-full">
-      <HeroSection />
+      <BgGradient />
+      <div className="flex flex-col">
+        <HeroSection />
+      </div>
+
       {/* <DemoSection /> */}
       {/* <HowItWorksSection /> */}
       {/* <PricingSection /> */}
--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Button } from "../ui/button";
-import { Sparkles } from "lucide-react";
+import { ArrowRight, Sparkles } from "lucide-react";
 import { Badge } from "../ui/badge";
+import Link from "next/link";
 
 const HeroSection = () => {
   return (
     <section>
-      <div className="">
+      <div className="relative mx-auto flex flex-col z-0 items-center justify-center py-16 sm:py-20 lg:pb-28 transition-all animate-in lg-px-12 max-w-7xl">
         <div className="flex">
           <div className="relative p-[1px] overflow-hidden rounded-full bg-linear-to-r from-rose-200 via-rose-500 to-rose-800 animate-gradient-x group">
             <Badge
@@ -19,9 +20,29 @@ const HeroSection = () => {
           </div>
         </div>
 
-        <h1>Transform PDF's into concise summaries</h1>
-        <h2>Get a beautiful summaries reel of the document in seconds.</h2>
-        <Button>Try Sommaire</Button>
+        <h1 className="font-bold py-6 text-center">
+          Transform PDF's into{" "}
+          <span className="relative inline-block">
+            <span className="relative z-10 px-2">concise </span>
+            <span
+              className="absolute inset-0 bg-rose-200/50 -rotate-2 rounded-lg transform -skew-y-1"
+              aria-hidden="true"
+            ></span>
+          </span>
+          summaries
+        </h1>
+        <h2 className="text-lg sm:text-xl lg:text-2xl text-center px-4 lg:px-0 lg:max-w-4xl text-gray-600">
+          Get a beautiful summaries reel of the document in seconds.
+        </h2>
+        <Button
+          variant={"link"}
+          className="text-white mt-6 text-base sm:text-lg lg:text-xl rounded-full px-8 sm:px-10 lg:px-12 py-6 sm:py-7 lg:py-8 lg:mt-16 bg-linear-to-r from-slate-900 to-rose-500 hover:from-rose-500 hover:to-slate-900 font-bold hover:no-underline shadow-lg transition-all duration-300"
+        >
+          <Link href="/#pricing" className="flex gap-2 items-center">
+            <span>Try Sommaire</span>
+            <ArrowRight className="animate-pulse" />
+          </Link>
+        </Button>
       </div>
     </section>
   );
